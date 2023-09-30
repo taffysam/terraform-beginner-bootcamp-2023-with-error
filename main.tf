17277a22fa3e45bbf4b891e142ca051dac086c16
taffysam <tdzomba@gmail.com>
2023-09-30 16:11:37 +0000

    #10 Running the Random Bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_providers {
+   aws = {
+      source = "hashicorp/aws"
+     version = "5.18.1"
+    }
+  
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
 
+resource "random_string" "bucket_name" {
+  length  = 16
+  special = false
+}
+
+output "random_bucket_id" {
+    value = random_string.bucket_name.id
+  
+}
+
+output "random_bucket_name" {
+    value = random_string.bucket_name.result
+  
+}
