62464c75c9a663c5694b510ec01056979a053726
taffysam <tdzomba@gmail.com>
2023-10-01 13:48:28 +0000

    Variables still not working for lowercase

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "user_uuid" {
 
    validation {
       condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
-     error_message = "user_uuid must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
+     error_message  = "user_uuid must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
 
 }
 
