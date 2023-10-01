3b084d351f11fe678cb629cb61f7731d2f6327ce
taffysam <tdzomba@gmail.com>
2023-10-01 14:31:33 +0000

    #23 Working Variables

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-resource "random_string" "bucket_name" {
-  length  = 32
-  special = false
-}
+#resource "random_string" "bucket_name" {
+#  length  = 32
+#  special = false
+#}
+
+#resource "aws_s3_bucket" "bootcamp" {
+#  bucket = random_string.bucket_name.result
+
+#  tags = {
+#  Useruuid = var.user_uuid
+#  }
+#}
 
 resource "aws_s3_bucket" "bootcamp" {
-  bucket = random_string.bucket_name.result
+  bucket = "87e8fc20-5f21-4b38-872b-ab8adfb49ed5"
 
   tags = {
   Useruuid = var.user_uuid
   }
 }
-
