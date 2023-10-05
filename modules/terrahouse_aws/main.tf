01189822d6c944a56861753b6228e12fe38d0ea9
Tafadzwa <tdzomba@gmail.com>
2023-10-05 10:11:15 +0200

    Updating bucket values

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -7,9 +7,9 @@ resource "random_string" "bucket_name" {
   special = false
 }
 
-resource "aws_s3_bucket" "bootcamp3" {
+#resource "aws_s3_bucket" "bootcamp3" {
 #  bucket = random_string.bucket_name.result
-}
+#}
 #  tags = {
 #  Useruuid = var.user_uuid
 #  }
@@ -19,9 +19,9 @@ resource "aws_s3_bucket" "bootcamp3" {
 #  bucket = "87e8fc20-5f21-4b38-872b-ab8adfb49ed5"
 #}
 
-resource "aws_s3_bucket" "bootcamp2" {
-  bucket = "22e8fc20-5f21-4b38-872b-ab8adfb49ed5"
-}
+#resource "aws_s3_bucket" "bootcamp2" {
+#  bucket = "22e8fc20-5f21-4b38-872b-ab8adfb49ed5"
+#}
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
 bucket = "87e8fc20-5f21-4b38-872b-ab8adfb49ed5"
@@ -31,23 +31,23 @@ bucket = "87e8fc20-5f21-4b38-872b-ab8adfb49ed5"
 resource "aws_s3_object" "object" {
   bucket = "87e8fc20-5f21-4b38-872b-ab8adfb49ed5"
   key    = "index.html"
-  source = "c:/Sofwares/Terraform Projects\terraform-beginner-bootcamp-2023-1/modules\terrahouse_aws/public/index.html"
+  source = "${path.root}/public.index.html"
+  #source = "c:/Sofwares/Terraform Projects/terraform-beginner-bootcamp-2023-1/modules/terrahouse_aws/public/index.html"
 
-  #source = "/workspace/terraform-begginner-bootcamp-2023/modules/terrahouse_aws/main.tf"
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
+    # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   # etag = filemd5("path/to/file")
 }
 
- # index_document {
- #   suffix = "index.html"
- # }
+  
+#index_document {
+#    suffix = "index.html"
+#  }
 
- # error_document {
- #   key = "error.html"
- # }
+#error_document {
+#    key = "error.html"
+#  }
 
 
 
