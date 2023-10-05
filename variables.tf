c99afbc5b1ddb4393217b3d3b6bd395b3d3e830b
Tafadzwa <tdzomba@gmail.com>
2023-10-05 14:35:30 +0200

    #Updating Variables

--- variables.tf
+++ variables.tf
@@ -10,22 +10,43 @@ variable "user_uuid" {
 
 }
 
+#variable "error_html_file_path" {
+#  description = "Path to the index.html file"
+#  type        = string
+
+#  validation {
+#    condition     = can(fileexists(var.error_file_path))
+#    error_message = "The specified index.html file path does not exist."
+#  }
+
+
 variable "index_html_file_path" {
   description = "Path to the index.html file"
   type        = string
 
   validation {
-    condition     = can(fileexists(var.index_html_file_path))
-    error_message = "The specified index.html file path does not exist."
+    condition     = length(var.index_html_file_path) > 0
+    error_message = "The specified index.html file path must not be empty."
   }
 }
 
-variable "error_html_file_path" {
+variable "error_file_path" {
   description = "Path to the index.html file"
   type        = string
 
   validation {
-    condition     = can(fileexists(var.error_file_path))
-    error_message = "The specified index.html file path does not exist."
+    condition     = length(var.error_file_path) > 0
+    error_message = "The specified index.html file path must not be empty."
   }
 }
+
+resource "null_resource" "validate_file_path" {
+  triggers = {
+    index_html_file_path = var.error_file_path
+    
+      }
+
+
+}
+
+
