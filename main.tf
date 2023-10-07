5842e9b476fed8e4446adc5f92eda9d89ad2b284
Tafadzwa Dzomba <dzombasamson@gmail.com>
2023-10-07 19:43:07 +0000

    Updating Providers

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 terraform {
-  
+
 }
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
 #  bucket_name = var.bucket_name
-# validate_file_path.index_html_file_path = index_html_file_path
-index_html_file_path = "c:/Sofwares/Terraform Projects/terraform-beginner-bootcamp-2023-1/modules/terrahouse_aws/public/index.html"
-#error_file_path = var.error_html_file_path
-error_html_file_path = "c:/Sofwares/Terraform Projects/terraform-beginner-bootcamp-2023-1/modules/terrahouse_aws/public/error#.html"
+  index_html_file_path = "c:/Sofwares/Terraform Projects/terraform-beginner-bootcamp-2023-1/modules/terrahouse_aws/public/index.html"
+  error_html_file_path = "c:/Sofwares/Terraform Projects/terraform-beginner-bootcamp-2023-1/modules/terrahouse_aws/public/error#.html"
 }
