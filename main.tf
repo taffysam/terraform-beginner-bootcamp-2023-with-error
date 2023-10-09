c4778dccad2ef30f2953132d756979a4f7e74b10
Tafadzwa Dzomba <dzombasamson@gmail.com>
2023-10-09 13:40:23 +0000

    #47 Updating CDN

--- main.tf
+++ main.tf
@@ -8,4 +8,5 @@ module "terrahouse_aws" {
 # validate_file_path.index_html_file_path = index_html_file_path
   index_html_file_path = var.index_html_file_path
   error_html_file_path = var.error_html_file_path
+ 
 }
